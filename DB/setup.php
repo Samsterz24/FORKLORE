3a385e075e89a51ce0ce3609d70f3462f5c39e24
Samsterz24 <samacadz124525@gmail.com>
2026-09-06 18:44:29 +0800

    added a new table in setup:'recipes'

--- DB/setup.php
+++ DB/setup.php
@@ -22,7 +22,7 @@ if ($conn->query($sql_db) === TRUE) {
 $conn->select_db("FORKLORE");
 
 
-$sql_table = "CREATE TABLE IF NOT EXISTS users (
+$sql_table1 = "CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -31,8 +31,18 @@ $sql_table = "CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )";
 
-if ($conn->query($sql_table) === TRUE) {
-    echo "Table 'users' created or already exists.<br>";
+$sql_table2 = "CREATE TABLE IF NOT EXISTS recipes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    category VARCHAR(50) DEFAULT 'General',
+    image_filename VARCHAR(255) DEFAULT 'default.jpg',
+    data_file VARCHAR(255) NULL, -- Path to a .json or .txt file containing full instructions
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- if ever deleting a user, will delete first the recipes na associated sa user
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+if ($conn->query($sql_table1 && $sql_table2) === TRUE) {
+    echo "Table 'users' and 'recipes' created or already exists.<br>";
 } else {
     echo "Error creating table: " . $conn->error;
 }
